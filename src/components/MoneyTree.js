7ed0800018921a77dc4dd628749a3568280f14c4
Ishank goel <ishankgoel811@gmail.com>
2021-06-10 15:37:49 +0530

    changes

--- src/components/MoneyTree.js
+++ src/components/MoneyTree.js
@@ -9,11 +9,29 @@ var [value, setvalue] = useState([])
 
     var num =5 ;
   const [time , setime] = useState(num) ;
-  const [rollSum , setrollsum] = useState(); 
-  const [dice1 , setdice1] = useState(3); 
+  const [rollSum , setrollsum] = useState(13); 
+  const [nextrollsum , setnextrollsum] = useState(""); 
+  const [dice1 , setdice1] = useState(5); 
   const [dice2 , setdice2] = useState(1);
-  const [dice3 , setdice3] = useState(9); 
- 
+  const [dice3 , setdice3] = useState(7); 
+ const [data , setdata] = useState({
+   3:"" ,
+   4:"" ,
+   5:"" , 
+   6:"" ,
+   7:"" ,
+   8:"" ,
+   9:"" ,
+   10:"" ,
+   11:"" ,
+   12:"" ,
+   13:"" ,
+   14:"" ,
+   15:"" ,
+   16:"" ,
+   17:"" ,
+   18:"" ,
+ })
   var timeref;
   let[numberofdraw , setnumberofdraw]  =useState(0) ; 
  
@@ -26,10 +44,6 @@ var [value, setvalue] = useState([])
     setdice3(dice3) ; 
     const sum = dice1+dice2+dice3 ; 
     setrollsum(sum); 
-
-
-
-    
   }
   var historydata = {
     dice1 , dice2 , dice3 , rollSum
@@ -37,15 +51,15 @@ var [value, setvalue] = useState([])
   
   function myFunction() {
     timeref = setInterval(function(){ 
+      
         num = num-1; 
         setime(num) 
-        if(num===0){
-            num=5;
+        if(num<1){
+             num=5;
             numberofdraw = numberofdraw+1
             setnumberofdraw(numberofdraw)
             clearInterval(timeref)
-            diceRoll();
-            
+            diceRoll();    
             myFunction(); 
         }
     },
@@ -53,11 +67,7 @@ var [value, setvalue] = useState([])
          
         
   }
-// useEffect(() => {
-  
-   
-    
-// },[num])
+
 useEffect(() => {
   dispatch(history(historydata)); 
   myFunction()
@@ -73,12 +83,21 @@ useEffect(() => {
   let lastElement4 = dicehistory[dicehistory.length - 2];
   let lastElement5 = dicehistory[dicehistory.length - 1];
   var val=[lastElement5,lastElement4,lastElement3, lastElement2,lastElement1]
+
 setvalue(val) ; 
 
 }, [numberofdraw])
+const numberClicked = (e)=>{
+   e.preventDefault();
+  const numbervalue = e.target.value ; 
+  console.log(e.target.value);
+  if(e.target.value==rollSum){
+    alert("write answer"); 
+  }
+}
 
     return (
-        <div>
+      <>
         <div style={{display:"flex" , flexDirection:"row"}}>
         <h1>{value[0]?.dice1}</h1>
         <h1>{value[0]?.dice2}</h1>
@@ -106,29 +125,165 @@ setvalue(val) ;
         </div>
         
        
-        
-<div className="dicegame">
-    <div className="timer">
-        <h1>Time left :- </h1>
-        <h2>{time}</h2>
-        </div>
-        <div className="dices">
-        <button>{dice1}</button>{"+"}
-        <button>{dice2}</button>{"+"}
-        <button>{dice3}</button>{"="}
-        <h3 className="sum">{rollSum}</h3>
-        </div>
-      
-        <div className="sum_number">
-        <h1>Number History</h1>
-<h1>2021{numberofdraw}</h1>
-        </div>
-       
-  
-
+        <div className="relative bg-gray-100 pb-48">      
+  <div className="flex p-3 justify-between">
+  <div className="flex pt-10 pl-5">
+    <div className="text-gray-900">
+      <button className="p-1 text-white pl-3 pr-3 bg-gray-700 rounded-full">{dice1}</button>{"+"}
+          <button className="p-1 text-white pl-3 pr-3 bg-gray-700 rounded-full">{dice2}</button>{"+"}
+          <button className="p-1 text-white pl-3 pr-3 bg-gray-700 rounded-full">{dice3}</button>{"="}
     </div>
+      <div className="text-white p-1 pr-3 pl-3 bg-gray-700 rounded-md h-8">{rollSum}</div>
+  </div>
+        <div className="mt-2 mr-3">
+          <div className="flex">
+            <h1 className="text-lg text-gray-600">Draw no:</h1>
+            <h1 className="font-semibold pl-2 text-lg">2021{numberofdraw}</h1>
+          </div>
+          <div className="flex text-2xl font-semibold">
+              <h2>00:00:{time}</h2>
+          </div>
+          <div className="flex mt-2">
+            <button className="border-red-500 text-red-500 mr-1">Rule</button>
+            <button className="bg-red-500 text-white p-1 rounded-md pl-2 pr-2">Buy record</button>
+          </div>
+        </div>
+      </div>
+      <div>
+        <div className="mt-5">
+          <h1 className="text-lg pl-5 p-1 font-medium bg-gray-700 text-white mb-1">Sum Type</h1> 
+          <div className="grid grid-flow-col">
+            <div className="inline-block mr-2 ml-2 p-2 bg-gray-800 rounded-lg items-center">
+              <h1 className="text-yellow-600 text-md">LARGE</h1>
+              <p className="text-white text-sm">1.9</p>
+            </div>
+            <div value="small" className="inline-block mr-2 p-2 bg-gray-800 rounded-lg items-center">
+              <h1 className="text-yellow-600 text-md">SMALL</h1>
+              <p className="text-white text-sm">1.9</p>
+            </div>
+            <div className="inline-block mr-2 p-2 bg-gray-800 rounded-lg items-center">
+              <h1 className="text-yellow-600 text-md">ODD</h1>
+              <p className="text-white text-sm">1.9</p>
+            </div>
+            <div className="inline-block mr-2 p-2 bg-gray-800 rounded-lg items-center">
+              <h1 className="text-yellow-600 text-md">EVEN</h1>
+              <p className="text-white text-sm">1.9</p>
+            </div>
+          </div>
+        </div>
+        <div className="pt-4">
+          <h1 className="text-lg bg-gray-700 pl-5 p-1 mb-1 font-medium text-white">Sum Number</h1>
+          <div className="grid grid-flow-row">
+            <div className="grid grid-flow-col mb-2">
+              <div className="inline-block ml-2 mr-1 p-2 bg-gray-800 rounded-lg items-center">
+                <div className="rounded-full p-1 pl-3 pr-3 border w-8 items-center ml-6">
+                  <button value={3} name="3"  onClick={numberClicked} className="text-yellow-600 text-md">3</button>
+                </div>
+                <p className="text-white ml-7 mt-1 text-sm">1.9</p>
+              </div>
+              <div className="inline-block ml-1 mr-1 p-2 bg-gray-800 rounded-lg items-center">
+                <div className="rounded-full p-1 pl-3 pr-3 border w-8 items-center ml-6">
+                  <button value={4} name="4"  onClick={numberClicked} className="text-yellow-600 text-md">4</button>
+                </div>
+                <p className="text-white ml-7 mt-1 text-sm">1.9</p>
+              </div>
+              <div className="inline-block ml-1 mr-1 p-2 bg-gray-800 rounded-lg items-center">
+                <div className="rounded-full p-1 pl-3 pr-3 border w-8 items-center ml-6">
+                  <button value={5} name="5"  onClick={numberClicked} className="text-yellow-600 text-md">5</button>
+                </div>
+                <p className="text-white ml-7 mt-1 text-sm">1.9</p>
+              </div>
+              <div className="inline-block ml-1 mr-1 p-2 bg-gray-800 rounded-lg items-center">
+                <div className="rounded-full p-1 pl-3 pr-3 border w-8 items-center ml-6">
+                  <button value={6} name="6"  onClick={numberClicked} className="text-yellow-600 text-md">6</button>
+                </div>
+                <p className="text-white ml-7 mt-1 text-sm">1.9</p>
+              </div>
+            </div>
+            <div className="grid grid-flow-col mb-2">
+              <div className="inline-block ml-2 mr-1 p-2 bg-gray-800 rounded-lg items-center">
+                <div className="rounded-full p-1 pl-3 pr-3 border w-8 items-center ml-6">
+                  <button value={7} name="7"  onClick={numberClicked} className="text-yellow-600 text-md">7</button>
+                </div>
+                <p className="text-white ml-7 mt-1 text-sm">1.9</p>
+              </div>
+              <div className="inline-block ml-1 mr-1 p-2 bg-gray-800 rounded-lg items-center">
+                <div className="rounded-full p-1 pl-3 pr-3 border w-8 items-center ml-6">
+                  <button value={8} name="8"  onClick={numberClicked} className="text-yellow-600 text-md">8</button>
+                </div>
+                <p className="text-white ml-7 mt-1 text-sm">1.9</p>
+              </div>
+              <div className="inline-block ml-1 mr-1 p-2 bg-gray-800 rounded-lg items-center">
+                <div className="rounded-full p-1 pl-3 pr-3 border w-8 items-center ml-6">
+                  <button value={9} name="9"  onClick={numberClicked} className="text-yellow-600 text-md">9</button>
+                </div>
+                <p className="text-white ml-7 mt-1 text-sm">1.9</p>
+              </div>
+              <div className="inline-block ml-1 mr-1 p-2 bg-gray-800 rounded-lg items-center">
+                <div className="rounded-full p-1 pl-3 pr-3 border w-8 items-center ml-6">
+                  <button value={10} name="10"  onClick={numberClicked} className="text-yellow-600 text-md">10</button>
+                </div>
+                <p className="text-white ml-7 mt-1 text-sm">1.9</p>
+              </div>
+            </div>
+            <div className="grid grid-flow-col mb-2">
+              <div className="inline-block ml-2 mr-1 p-2 bg-gray-800 rounded-lg items-center">
+                <div className="rounded-full p-1 pl-3 pr-3 border w-8 items-center ml-6">
+                  <button value={11} name="11"  onClick={numberClicked} className="text-yellow-600 text-md">11</button>
+                </div>
+                <p className="text-white ml-7 mt-1 text-sm">1.9</p>
+              </div>
+              <div className="inline-block ml-1 mr-1 p-2 bg-gray-800 rounded-lg items-center">
+                <div className="rounded-full p-1 pl-3 pr-3 border w-8 items-center ml-6">
+                  <button value={12} name="12"  onClick={numberClicked} className="text-yellow-600 text-md">12</button>
+                </div>
+                <p className="text-white ml-7 mt-1 text-sm">1.9</p>
+              </div>
+              <div className="inline-block ml-1 mr-1 p-2 bg-gray-800 rounded-lg items-center">
+                <div className="rounded-full p-1 pl-3 pr-3 border w-8 items-center ml-6">
+                  <button value={13} name="13"  onClick={numberClicked} className="text-yellow-600 text-md">13</button>
+                </div>
+                <p className="text-white ml-7 mt-1 text-sm">1.9</p>
+              </div>
+              <div className="inline-block ml-1 mr-1 p-2 bg-gray-800 rounded-lg items-center">
+                <div className="rounded-full p-1 pl-3 pr-3 border w-8 items-center ml-6">
+                  <button value={14} name="14"  onClick={numberClicked} className="text-yellow-600 text-md">14</button>
+                </div>
+                <p className="text-white ml-7 mt-1 text-sm">1.9</p>
+              </div>
+            </div>
+            <div className="grid mb-2 grid-flow-col">
+              <div className="inline-block ml-2 mr-1 p-2 bg-gray-800 rounded-lg items-center">
+                <div className="rounded-full p-1 pl-3 pr-3 border w-8 items-center ml-6">
+                  <button value={15} name="15"  onClick={numberClicked} className="text-yellow-600 text-md">15</button>
+                </div>
+                <p className="text-white ml-7 mt-1 text-sm">1.9</p>
+              </div>
+              <div className="inline-block ml-1 mr-1 p-2 bg-gray-800 rounded-lg items-center">
+                <div className="rounded-full p-1 pl-3 pr-3 border w-8 items-center ml-6">
+                  <button value={16} name="16"  onClick={numberClicked} className="text-yellow-600 text-md">16</button>
+                </div>
+                <p className="text-white ml-7 mt-1 text-sm">1.9</p>
+              </div>
+              <div className="inline-block ml-1 mr-1 p-2 bg-gray-800 rounded-lg items-center">
+                <div className="rounded-full p-1 pl-3 pr-3 border w-8 items-center ml-6">
+                  <button value={17} name="17"  onClick={numberClicked} className="text-yellow-600 text-md">17</button>
+                </div>
+                <p className="text-white ml-7 mt-1 text-sm">1.9</p>
+              </div>
+              <div className="inline-block ml-1 mr-1 p-2 bg-gray-800 rounded-lg items-center">
+                <div className="rounded-full p-1 pl-3 pr-3 border w-8 items-center ml-6">
+                  <button value={18} name="18"  onClick={numberClicked} className="text-yellow-600 text-md">18</button>
+                </div>
+                <p className="text-white ml-7 mt-1 text-sm">1.9</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
 </div>
-    )
+</>
+    )    
 }
 
 export default MoneyTree
--- src/redux/reducers/MoneyTree.js
+++ src/redux/reducers/MoneyTree.js
@@ -1,18 +1,17 @@
 const Moneytree = (history=[] , action)=>{
     switch(action.type){
         case "HISTORY":
-            console.log(action.payload);
-            console.log(history.length); 
+        
             
             // var flim = history.slice(0,5,-1).map((item)=>{
             //     console.log(item); 
             // })
-            let lastElement1 = history[history.length - 5]
-            let lastElement2 = history[history.length - 4]
-            let lastElement3 = history[history.length - 3];
-            let lastElement4 = history[history.length - 2];
-            let lastElement5 = history[history.length - 1];
-           var val=[lastElement1,lastElement2,lastElement3, lastElement4,lastElement5]
+        //     let lastElement1 = history[history.length - 5]
+        //     let lastElement2 = history[history.length - 4]
+        //     let lastElement3 = history[history.length - 3];
+        //     let lastElement4 = history[history.length - 2];
+        //     let lastElement5 = history[history.length - 1];
+        //    var val=[lastElement1,lastElement2,lastElement3, lastElement4,lastElement5]
            
    
             return [
